dde8e4b3363e0e32cf37b3ebc3786baeb02d0587
azwahnfi <azwa_hanafiah@yahoo.com>
2022-01-02 02:20:46 +0800

    update total score

--- app/src/main/java/com/example/savingprogress/TotalScore.java
+++ app/src/main/java/com/example/savingprogress/TotalScore.java
@@ -1,14 +1,24 @@
 package com.example.savingprogress;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mikhaellopez.circularprogressbar.CircularProgressBar;
+
 public class TotalScore extends AppCompatActivity {
+
+    CircularProgressBar circularProgressBar;
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.totalscore;
-}
+        setContentView(R.layout.totalscore);
+
+        circularProgressBar = findViewById(circularProgressBar);
+        resultText = findViewById(resultText);
+
+    }
 }
