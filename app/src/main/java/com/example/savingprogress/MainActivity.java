9f388133361998643a90fd95c34b2c1afee6130b
azwahnfi <azwa_hanafiah@yahoo.com>
2022-01-01 14:52:37 +0800

    Testing

---
+++ app/src/main/java/com/example/savingprogress/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.savingprogress;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSurvey();
+            }
+        });
+    }
+    public void openSurvey(){
+        Intent intent = new Intent(this, Survey.class);
+        startActivity(intent);
+    }
+}
