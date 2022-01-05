135a07010ddb6c7ae003f290ae70bb2084e0c03c
azwahnfi <azwa_hanafiah@yahoo.com>
2022-01-06 01:30:39 +0800

    update result text

--- app/src/main/java/com/example/savingprogress/TotalScore.java
+++ app/src/main/java/com/example/savingprogress/TotalScore.java
@@ -10,6 +10,7 @@ import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 public class TotalScore extends AppCompatActivity {
 
     CircularProgressBar circularProgressBar;
+    TextView score;
     TextView resultText;
     int count0, count1, count2, count3;
 
@@ -23,8 +24,24 @@ public class TotalScore extends AppCompatActivity {
         count2.getIntent().getIntExtra("count2", 0);
         count3.getIntent().getIntExtra("count3", 0);
 
-        circularProgressBar = findViewById(circularProgressBar);
-        resultText = findViewById(resultText);
+        circularProgressBar = findViewById(R.id.circularProgressBar);
+        score = findViewById(R.id.score);
+        resultText = findViewById(R.id.resultText);
+
+        switch(score)
+        {
+            case 0: t.setText("Low Dependence");
+            break;
+
+            case 1: t.setText("Low to Mod Dependence");
+            break;
+
+            case 2: t.setText("Moderate Dependence");
+            break;
+
+            case 3: t.setText("High Dependence");
+            break;
+        }
 
     }
 }
